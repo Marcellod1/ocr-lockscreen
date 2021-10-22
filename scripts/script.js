2158ff8939b5966660a5e71d55acd477f1006e4b
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-10-21 19:47:27 -0600

    handwriting library integration and character unicode recogniton

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,41 @@
 $(document).ready(function(){
-    var can = new handwriting.Canvas(document.getElementById("can"), 10);
+    // Create Canvas
+    var canvas = new handwriting.Canvas(document.getElementById("canvas"));
+    
+    // Set error callback function
+    canvas.setCallBack(function(data, err) {
+        if(err) throw err;
+        else console.log(data);
+    });
+
+    // Set options
+    canvas.setLineWidth(15);
+    canvas.setOptions(
+        {
+            language: "ja",
+            numOfReturn: 3
+        }
+    );
+
+    // Submit button event
+    $("#submit-button").mousedown(function(e){
+        console.log("meme");
+            var meme = canvas.recognize();
+        }
+    );
+
+    // Submit button event
+    $("#reset-button").mousedown(function(e){
+        console.log("meme");
+            var meme = canvas.erase();
+        }
+    );
+
+
+
+
+
+
+
+
 });
