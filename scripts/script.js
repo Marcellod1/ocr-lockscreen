e95dfa92a14187dcfbfa6bffd2166f43ea2c3ec3
Marcello Di Benedetto <marcellodb17@gmail.com>
2021-10-24 14:46:25 -0600

    removed dead code, moved dictionaries to their own file, put main program implemenataion within the handwriting lib's callback

--- scripts/script.js
+++ scripts/script.js
@@ -1,86 +1,37 @@
 $(document).ready(function(){
 
-    var hiragana = {
-        "a":"あ" , "i":"い" ,  "e":"え", "o":"お" , "u":"う",
-        "ka":"か" , "ki":"き" ,  "ke":"け", "ko":"こ" , "ku":"く",
-        "sa":"さ" , "shi":"し" ,  "se":"せ", "so":"そ" , "su":"す",
-        "ta":"た" , "chi":"ち" ,  "te":"て", "to":"と" , "tsu":"つ",
-        "na":"な" , "ni":"に" ,  "ne":"ね", "no":"の" , "nu":"ぬ",
-        "ha":"は" , "hi":"ひ" ,  "he":"へ", "ho":"ほ" , "fu":"ふ",
-        "ma":"ま" , "mi":"み" ,  "me":"め", "mo":"も" , "mu":"む",
-        "ya":"や", "yo":"よ" , "yu":"ゆ",
-        "ra":"ら" , "ri":"り" ,  "re":"れ", "ro":"ろ" , "ru":"る",
-        "wa":"わ" , "wo":"を",
-        "n":"ん"
-    }
-
-    var katagana = {
-    "a":"ア" , "i":"イ" ,  "e":"エ", "o":"オ" , "u":"ウ",
-    "ka":"カ" , "ki":"キ" ,  "ke":"ケ", "ko":"コ" , "ku":"ク",
-    "sa":"サ" , "shi":"シ" ,  "se":"セ", "so":"ソ" , "su":"ス",
-    "ta":"タ" , "chi":"チ" ,  "te":"テ", "to":"ト" , "tsu":"ツ",
-    "na":"ナ" , "ni":"ニ" ,  "ne":"ネ", "no":"ノ" , "nu":"ヌ",
-    "ha":"ハ" , "hi":"ヒ" ,  "he":"ヘ", "ho":"ホ" , "fu":"フ",
-    "ma":"マ" , "mi":"ミ" ,  "me":"メ", "mo":"モ" , "mu":"ム",
-    "ya":"ヤ", "yo":"ヨ" , "yu":"ユ",
-    "ra":"ラ" , "ri":"リ" ,  "re":"レ", "ro":"ロ" , "ru":"ル",
-    "wa":"ワ" , "wo":"ヲ",
-    "n":"ン"
-    }
-
-    var hiraTenTen = {
-        "ga":"が" , "gi":"ぎ" ,  "ge":"げ", "go":"ご" , "gu":"ぐ",
-        "za":"ざ" , "zhi":"じ" ,  "ze":"ぜ", "zo":"ぞ" , "zu":"ず",
-        "da":"だ" , "di":"ぢ" ,  "de":"で", "do":"ど" , "du":"づ",
-        "ba":"ば" , "bi":"び" ,  "be":"べ", "bo":"ぼ" , "bu":"ぶ",
-        "pa":"ぱ" , "pi":"ぴ" ,  "pe":"ぺ", "po":"ぽ" , "pu":"ぷ"
-    }
-
-    var kataTenTen = {
-        "ga":"ガ" , "gi":"ギ" ,  "ge":"ゲ", "go":"ゴ" , "gu":"グ",
-        "za":"ザ" , "zhi":"ジ" ,  "ze":"ゼ", "zo":"ゾ" , "zu":"ズ",
-        "da":"ダ" , "di":"ヂ" ,  "de":"デ", "do":"ド" , "du":"ヅ",
-        "ba":"バ" , "bi":"ビ" ,  "be":"ベ", "bo":"ボ" , "bu":"ブ",
-        "pa":"パ" , "pi":"ピ" ,  "pe":"ペ", "po":"ポ" , "pu":"プ"
-    }
-
-    // Create Canvas
+    // Create Canvas and set options
     var canvas = new handwriting.Canvas(document.getElementById("canvas"));
-    
-    // Set error callback function
-    canvas.setCallBack(function(data, err) {
-        if(err) throw err;
-        else console.log(data);
-    });
-
-    // Set options
     canvas.setLineWidth(15);
     canvas.setOptions(
         {
             language: "ja",
-            numOfReturn: 3
+            numOfReturn: 1
         }
     );
 
-    // Submit button event
+    /* Set callback function for canvas.recognize. This function is called after the response from Google's API endpoint is complete 
+    and throws errors if any occur.
+
+    This function will perform the check between the user's input and the prompt
+    */
+    canvas.setCallBack(function(data, err) {
+        if(err) throw err;
+        else{
+            console.log(data);
+        }
+    });
+
+
+    // Simple submit button event
     $("#submit-button").mousedown(function(e){
-        console.log("meme");
-            var meme = canvas.recognize();
+           canvas.recognize();
         }
     );
 
-    // Submit button event
+    // Simple reset button event
     $("#reset-button").mousedown(function(e){
-        console.log("meme");
-            var meme = canvas.erase();
+            canvas.erase();
         }
     );
-
-
-
-
-
-
-
-
 });
